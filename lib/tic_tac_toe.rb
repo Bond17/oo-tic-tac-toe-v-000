0d5b03490fb3c13d7f609e51cf6efd20ea953634
Robert Watson <spartan83@gmail.com>
2017-12-03 22:06:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,5 +39,8 @@ class TicTacToe
   def position_taken?(location)
     @board[location] != " " && @board[location] != ""
   end
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
 
 end
