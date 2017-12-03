450cc6692384c7a1bb5c4b0cb1bd288adb3f109d
Robert Watson <spartan83@gmail.com>
2017-12-03 22:04:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,5 +29,13 @@ class TicTacToe
      puts "-----------"
      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
    end
+  def input_to_index(user_input)
+     user_input.to_i - 1
+  end
+
+  def move(index, player)
+    @board[index] = player
+  end
+
 
 end
